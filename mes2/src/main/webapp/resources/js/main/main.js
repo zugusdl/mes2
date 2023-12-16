e195fd30ef9971df46276efbc4022274117f9209
JuhyeonLee1 <zugusdl@gmail.com>
2023-12-16 15:27:28 +0900

    메인 차트 추가

---
+++ mes2/src/main/webapp/resources/js/main/main.js
@@ -0,0 +1,88 @@
+/* Created by Tivotal */
+
+let sideMenu = document.querySelectorAll(".nav-link");
+sideMenu.forEach((item) => {
+  let li = item.parentElement;
+
+  item.addEventListener("click", () => {
+    sideMenu.forEach((link) => {
+      link.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
+});
+
+let menuBar = document.querySelector(".menu-btn");
+let sideBar = document.querySelector(".sidebar");
+menuBar.addEventListener("click", () => {
+  sideBar.classList.toggle("hide");
+});
+
+let switchMode = document.getElementById("switch-mode");
+switchMode.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
+
+let searchFrom = document.querySelector(".content nav form");
+let searchBtn = document.querySelector(".search-btn");
+let searchIcon = document.querySelector(".search-icon");
+searchBtn.addEventListener("click", (e) => {
+  if (window.innerWidth < 576) {
+    e.preventDefault();
+    searchFrom.classList.toggle("show");
+    if (searchFrom.classList.contains("show")) {
+      searchIcon.classList.replace("fa-search", "fa-times");
+    } else {
+      searchIcon.classList.replace("fa-times", "fa-search");
+    }
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 576) {
+    searchIcon.classList.replace("fa-times", "fa-search");
+    searchFrom.classList.remove("show");
+  }
+  if (window.innerWidth < 768) {
+    sideBar.classList.add("hide");
+  }
+});
+
+if (window.innerWidth < 768) {
+  sideBar.classList.add("hide");
+}
+
+$('.feat-btn').click(function(){
+	$('ul .feat-show').toggleClass("");
+	
+	
+});
+
+// 차트
+const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yValues = [55, 49, 44, 24, 15];
+const barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("sales-chart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "World Wine Production 2018"
+    }
+  }
+});
+
+
